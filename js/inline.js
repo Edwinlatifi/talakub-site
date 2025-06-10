aca1713e18ed10efa510e9d8cc2906cc90ba0116
Edwinlatifi <uniqwallet@gmail.com>
2025-06-11 02:20:56 +0330

    Redesign info-section and price-section layout; fix CSP; add missing components

--- js/inline.js
+++ js/inline.js
@@ -1,2 +1,2 @@
 ﻿console.log("Inline script test");
-// لطفاً اسکریپت inline (مثلاً خط 119 index.html) رو بفرست
+// لطفاً خروجی Get-Content F:\talakub-site\index.html -TotalCount 50 رو بفرست تا اسکریپت inline رو پیدا کنیم
